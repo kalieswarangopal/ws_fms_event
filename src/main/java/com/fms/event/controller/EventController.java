d01094aded97f969f2fe30e83e271a28df25610a
Kalieswaran Gopal <kalieswaran.gopal@cognizant.com>
2020-04-21 05:04:59 -0700

    Excel download changes

--- src/main/java/com/fms/event/controller/EventController.java
+++ src/main/java/com/fms/event/controller/EventController.java
@@ -1,15 +1,16 @@
 package com.fms.event.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import com.fms.event.dto.DashboardResponseDTO;
 import com.fms.event.entity.Event;
 import com.fms.event.service.EventService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class EventController {
@@ -25,8 +26,14 @@ public class EventController {
 
 	@GetMapping("/dashboard")
 	public Mono<DashboardResponseDTO> getDashboardInfo() {
-		
+
 		return eventService.getDashboardInfo();
 	}
-	
+
+	@GetMapping("/report")
+	public Mono<ResponseEntity> downloadExcel() {
+
+		return eventService.downloadExcel();
+	}
+
 }
--- src/main/java/com/fms/event/service/EventService.java
+++ src/main/java/com/fms/event/service/EventService.java
@@ -1,15 +1,22 @@
 package com.fms.event.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.io.ByteArrayInputStream;
+import java.util.List;
 
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.fms.event.dto.DashboardResponseDTO;
 import com.fms.event.entity.Event;
+import com.fms.event.helper.ExcelDownloadHelper;
 import com.fms.event.repository.EmployeeRepository;
 import com.fms.event.repository.EventRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventService {
@@ -19,6 +26,9 @@ public class EventService {
 
 	@Autowired
 	EmployeeRepository employeeRepository;
+
+	@Autowired
+	ExcelDownloadHelper excelDownloadHelper;
 
 	public Flux<Event> getAllEvents() {
 
@@ -33,16 +43,26 @@ public class EventService {
 		Mono<Integer> totalVolunteers = eventRepository.getTotalVolunteers();
 		Mono<Long> totalParticipants = employeeRepository.count();
 
-		return Mono.zip(totalEvents, livesImpacted, totalVolunteers,
-				totalParticipants).flatMap(
-				data -> {
-					data.getT1();
-					data.getT2();
-					data.getT3();
-					data.getT4();
-					return Mono.just(new DashboardResponseDTO(data.getT1(),
-							data.getT2(), data.getT3(), data.getT4()));
-				});
+		return Mono.zip(totalEvents, livesImpacted, totalVolunteers, totalParticipants).flatMap(data -> {
+			data.getT1();
+			data.getT2();
+			data.getT3();
+			data.getT4();
+			return Mono.just(new DashboardResponseDTO(data.getT1(), data.getT2(), data.getT3(), data.getT4()));
+		});
+
+	}
+
+	public Mono<ResponseEntity> downloadExcel() {
+
+		Mono<List<Event>> events = eventRepository.findAll().collectList();
+		return events.map(event -> {
+			ByteArrayInputStream stream = excelDownloadHelper.generateExcel(event);
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-Disposition", "attachment; filename=Events.xlsx");
+			headers.add("Content-Type", "application/octet-stream");
+			return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
+		});
 
 	}
 
